4e7594c1ec3c5b72d70545bdb28f5f69e66bb526
Anonymous <masalovilla@gmail.com>
2023-10-15 16:14:43 +0700

    Added a "multiply" function

--- Polynom.cpp
+++ Polynom.cpp
@@ -1,11 +1,11 @@
 #include "Grid.cpp"
 
 class Polynom {
-    public:
+    private:
      
      vector<double> polynom;
     
-    private:
+    public:
 
     Polynom(){
         polynom.push_back(0);
@@ -17,11 +17,42 @@ class Polynom {
         polynom.push_back(0);
     }
 
+    Polynom(vector<double> vector){
+        this->polynom = vector;
+    }
+
+     int getDegree(){
+        return this->polynom.size() - 1;
+    }
+
     double calculateValue(double x){
         double value = 0;
-        for(int i = 1; i < this->polynom.size(); ++i){
+        for(size_t i = 1; i < this->getDegree() + 1; ++i){
             value += polynom[i] * (x, i);
         }
         return value;
+    }
+
+    Polynom multiply(Polynom &polynom){
+        int newDegree = this->getDegree() + polynom.getDegree();
+        Polynom newPolynom = Polynom(newDegree);
+
+        double resultCoefficient;
+        for(size_t i = 0; i < newDegree + 1; ++i){
+            resultCoefficient = 0.0;
+            for(size_t j = 0; j <= i; ++j){
+                if(j < this->getDegree() + 1 && (i - j) < polynom.getDegree() + 1){
+                    resultCoefficient += this->polynom[j] * polynom.polynom[i - j];
+                }
+            }
+            newPolynom.polynom[i] = resultCoefficient;
+        }
+        return  newPolynom;
+    }
+
+    void print(){
+        for(size_t i = 0; i < this->getDegree() + 1; ++i){
+            std::cout << this->polynom[i] << " "; 
+        }
     }
 };
